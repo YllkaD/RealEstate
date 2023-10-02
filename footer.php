48db808ef9faa4752dedbec4c5fd66dea40dba1a
YllkaD <yllkadallku@gmail.com>
2023-10-02 11:32:47 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+    <footer>
+        <p>This is my footer</p>
+    </footer>
+
+    <?php wp_head(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Real Estate</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    
