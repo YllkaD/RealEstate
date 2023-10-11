dad641a332db6bb5186e893b9014be7c2e822aaa
hysengashii <71273409+hysengashii@users.noreply.github.com>
2023-10-11 17:19:15 +0200

    Add Image Module

---
+++ modules/image_module.php
@@ -0,0 +1,9 @@
+<?php
+$image = get_sub_field('image');
+?>
+
+<section>
+        <div class="flex justify-center items-center">
+            <img src="<?php echo $image; ?>" alt="<?php echo $image; ?>" class="h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer" >
+        </div>
+</section>
