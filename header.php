50762a3b5d1174ce0be1ecc8549488c83c366da0
Shqipdon22 <121349621+Shqipdon22@users.noreply.github.com>
2023-10-12 19:19:35 +0200

    header
    Header (first commit)

--- header.php
+++ header.php
@@ -10,21 +10,30 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
+
+
+
+      <div class="flex justify-center bg-white">
+        <img src="<?php echo get_stylesheet_directory_uri() . '/img/real-estate.svg'; ?>" class="w-20">
+      </div>
     
 
-<?php
 
-/*
-	==========================================
-	Pjesen e Headerit e rregullon,wp_nav_menu e kam vendos vetem per veti me mu shfaq
-	==========================================
-*/
-wp_nav_menu(
-    array(
-        'theme_location' => 'primary-menu',
-        'menu_id'        => 'primary-menu',
-    )
-);
+      <div class="flex justify-evenly bg-neutral-800 text-white">
+           <?php
+               wp_nav_menu(
+                    array(
+                   'theme_location' => 'primary-menu',
+                   'menu_id'        => 'primary-menu',
+                   
+                                     )
+                 );
+                 ?>
+         
+           <form role="search" method="get" class="flex items-center">
+             <input type="search" class="bg-gray-100 rounded-full px-4 py-2" placeholder="Search..." value="<?php echo get_search_query(); ?>" name="s">
+             <button type="submit" class="bg-neutral-800 text-white px-4 py-2 rounded-full ml-2">Search</button>
+            </form>
+     </div>
 
 
-?>
