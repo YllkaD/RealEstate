f1736d5bfd5aae43916e3a8ff94ce249dc24f07a
erispireva <63068660+erispireva@users.noreply.github.com>
2023-10-20 15:21:10 +0200

    Footer template

--- footer.php
+++ footer.php
@@ -1,8 +1,40 @@
     <footer>
-        <p>This is my footer</p>
+            <div class="footer-container">
+
+        <div class="footer-left">
+            <h3>Contact Us</h3>
+            <p>Email: contact@example.com</p>
+            <p>Phone: (123) 456-789</p>
+            <p>Address: 111 Street, City, Country</p>
+        </div>
+        <div class="footer-column">
+            <h3>About Us</h3>
+            <p>...........</p>
+        </div>
+        <div class="footer-column">
+            <h3>Quick Links</h3>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="footer-column">
+            <h3>Connect With Us</h3>
+            <ul class="social-icons">
+                <li><a href="#"><img src="facebook-icon.png" alt="Facebook"></a></li>
+                <li><a href="#"><img src="twitter-icon.png" alt="Twitter"></a></li>
+                <li><a href="#"><img src="instagram-icon.png" alt="Instagram"></a></li>
+            </ul>
+        </div>
+        </div>
+        <div class="copyright">
+        &copy; 2023 Real Estate. All rights reserved.
+</div>
     </footer>
 
-    <?php wp_head(); ?>
+    <?php wp_footer(); ?>
 
 
 
